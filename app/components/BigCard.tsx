91961d2e092b34b656e175004a26a9953c523d7e
vumg <ab17100222@gmail.com>
2023-06-10 19:54:30 -0600

    first graph

--- app/components/BigCard.tsx
+++ app/components/BigCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 interface CardProps {
     title: string;
     body: string;
@@ -8,7 +9,7 @@ interface CardProps {
 
 const BigCard: React.FC<CardProps> = ({ title, body, children }) => {
     return (
-        <div className="bg-black text-white border border-white rounded-lg p-5 m-5 text-white flex-shrink-0 w-96">
+        <div className="text-white border border-white rounded-lg p-5 m-5 text-white flex-shrink-0 w-96">
             <h2 className="text-lg font-bold mb-4">{title}</h2>
             <p className="mb-4">{body}</p>
             {children}
--- app/components/Central.tsx
+++ app/components/Central.tsx
@@ -11,7 +11,7 @@ const BigCardList: React.FC = () => {
                 body="In today's era of AI and advanced technologies, leveraging data visualization becomes crucial in delivering exceptional value to clients. Harnessing the power of data through visualization enables us to unlock actionable insights and drive impactful outcomes. Let me help you stand out by showcasing your expertise in data visualization and its transformative impact on client success.">
                 <Graph1/>    
             </BigCard>
-
+{/* 
             <BigCard
                 title="Geocentric Data"
                 body="In today's digital landscape, the effective visualization of geocentric data holds immense potential for delivering significant value to clients. By leveraging interactive maps and spatial analysis, we can unlock valuable insights, drive informed decision-making, and provide impactful solutions. Let me help you showcase your expertise in geocentric data visualization, demonstrating how it can create a competitive advantage for your clients and position you as a valuable asset in the industry.">
@@ -20,7 +20,7 @@ const BigCardList: React.FC = () => {
             <BigCard
                 title="Simulation"
                 body='The realm of simulation opens up a world of possibilities for tackling complex physics and intricate phenomena. By harnessing advanced computational models, we can create realistic simulations that enable us to understand, predict, and optimize intricate systems. Let me help you highlight your expertise in simulation, showcasing your ability to simulate and analyze complex scenarios to drive innovation and solve real-world challenges. Together, we can demonstrate the power of simulation in pushing boundaries and achieving remarkable outcomes.'>
-            </BigCard>
+            </BigCard> */}
 
             
         </div>
